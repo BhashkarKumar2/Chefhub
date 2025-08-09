9726fc79a07b228b677faa78aa63e0d4b734be66
BhashkarKumar2 <bhashkarkumar2063@gmail.com>
2025-08-09 15:35:37 +0530

    made changes in socket

--- backend/services/socketService.js
+++ backend/services/socketService.js
@@ -10,7 +10,7 @@ class SocketService {
   init(server) {
     this.io = new Server(server, {
       cors: {
-        origin: ["http://localhost:5173", "http://localhost:3000"],
+        origin: ["http://localhost:5173", "http://localhost:3000","https://chefhub-poou.vercel.app"],
         credentials: true
       }
     });
