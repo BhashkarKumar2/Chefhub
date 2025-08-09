9ac154aaf4141ec2000f1c96d5ac5dd77fe77ca5
BhashkarKumar2 <bhashkarkumar2063@gmail.com>
2025-08-09 15:47:05 +0530

    google callback changes made

--- backend/auth/Passport.js
+++ backend/auth/Passport.js
@@ -14,7 +14,10 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/api/auth/google/callback',
+       callbackURL:
+        process.env.NODE_ENV === "production"
+          ? "https://chefhub.onrender.com/api/auth/google/callback"
+          : "http://localhost:5000/api/auth/google/callback",
       scope: ['profile', 'email']
     },
     async (accessToken, refreshToken, profile, done) => {
